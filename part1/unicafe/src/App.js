5312efb9bb1d358e8cf592689cf8ec0c214da624
matej <matej.djerdji@gmail.com>
2023-06-13 18:30:59 +0200

    Exercise 1.7 - unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,9 +6,21 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
-const StatisticLine = ({stat, value}) => (
+const StatisticLine = ({ stat, value }) => (
   <div>{stat} {value}</div>
 )
+
+const CalculateStats = (good, neutral, bad) => {
+  const total = good + neutral + bad
+  const average = (good - bad) / total 
+  const positive = (good / total) * 100
+
+  return {
+    total: total,
+    average: average ? average : 0,
+    positive: positive ? positive : 0
+  }
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
@@ -27,16 +39,22 @@ const App = () => {
     setBad(bad + 1)
   }
 
+  const stats = CalculateStats(good, neutral, bad)
+
   return (
     <div>
       <h1>Give Feedback</h1>
-      <Button handleClick={handleGoodClick} text='Good'/>
-      <Button handleClick={handleNeutralClick} text='Neutral'/>
-      <Button handleClick={handleBadClick} text='Bad'/>
+      <Button handleClick={handleGoodClick} text='Good' />
+      <Button handleClick={handleNeutralClick} text='Neutral' />
+      <Button handleClick={handleBadClick} text='Bad' />
       <h1>Statistics</h1>
-      <StatisticLine stat='Good' value={good}/>
-      <StatisticLine stat='Neutral' value={neutral}/>
-      <StatisticLine stat='Bad' value={bad}/>
+      <StatisticLine stat='Good' value={good} />
+      <StatisticLine stat='Neutral' value={neutral} />
+      <StatisticLine stat='Bad' value={bad} />
+      <br />
+      <StatisticLine stat='All' value={stats.total} />
+      <StatisticLine stat='Average' value={stats.average} />
+      <StatisticLine stat='Positive' value={stats.positive + '%'} />
     </div>
   )
 }
