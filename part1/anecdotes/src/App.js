b2ac0b4d6eff56fb383b94eaaf0369f44559a695
matej <matej.djerdji@gmail.com>
2023-06-13 18:40:48 +0200

    Exercise 1.14 - anecdotes step3

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -6,6 +6,13 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min)
 }
 
+const Anecdote = ({ anecdote, votes }) => (
+  <div>
+    <p><em>{anecdote}</em></p>
+    <p>Has {votes} votes</p>
+  </div>
+)
+
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
     {text}
@@ -39,12 +46,27 @@ const App = () => {
     setScores(newScores)
   }
 
+  // Using Array.reduce() to find the index of the biggest score
+  // The accumulator (maxIdx) of the callback function is the index
+  // of the biggest score element (intially set to 0). On each call, 
+  // the callback function checks whether the current element value 
+  // is bigger than the value at 'array[maxIdx]', and if it is,
+  // returns the current index, otherwise returns the old accumulator
+  // value (maxIdx).
+  const maxScoreIndex = scores.reduce(
+    (maxIdx, val, i, array) => val >= array[maxIdx] ? i : maxIdx,
+    0
+  )
+
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
-      <p>Has {scores[selected]} votes</p>
+      <h1>Anecdote of the day</h1>
+      <Anecdote anecdote={anecdotes[selected]} votes={scores[selected]} />
       <Button handleClick={handleVoteClick} text='Vote' />
       <Button handleClick={handleNextAnecdoteClick} text='Next anecdote' />
+      <br />
+      <h1>Anecdote with most votes</h1>
+      <Anecdote anecdote={anecdotes[maxScoreIndex]} votes={scores[maxScoreIndex]} />
     </div>
   )
 }
